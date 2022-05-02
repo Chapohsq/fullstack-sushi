7fe5e4d91baeb76128d8d02e318caca5ca24a76d
Chapohsq <capogrecoale467@gmail.com>
2022-05-02 15:00:20 +0200

    fine app

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import App from "../App";
+
+function NavBar(){
+    return(
+        <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Fullstack Sushi</span>
+        </div>
+      </nav>
+    );
+}
+
+export default NavBar; 
